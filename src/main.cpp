a07a44488ab2b2226e61b259f7cce54cf37a8300
kraxarn <me@kraxarn.com>
2021-03-11 22:14:05 +0100

    Print SDL version for no reason

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <sstream>
 
 #include <SDL.h>
 #include <SDL_ttf.h>
 
 #include "game.hpp"
 
-int main()
+std::string sdl_linked_version()
+{
+	SDL_version version;
+	SDL_GetVersion(&version);
+
+	std::stringstream ss;
+	ss << std::to_string(version.major) << "."
+		<< std::to_string(version.minor) << "."
+		<< std::to_string(version.patch);
+	return ss.str();
+}
+
+int main(int argc, char **argv)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
 		std::cerr << "Init: " << SDL_GetError() << std::endl;
 		return 1;
 	}
+	std::cout << "SDL " << sdl_linked_version() << std::endl;
 
 	if (TTF_Init() != 0)
 	{
