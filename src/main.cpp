950a86ebe937cb6b0452f353281be09a733ba2d8
kraxarn <me@kraxarn.com>
2021-03-14 20:58:39 +0100

    Probably not good global try catch

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,23 @@
 #include "ce/engine.hpp"
 #include "ce/log.hpp"
+#include "ce/ui/messagebox.hpp"
 
 #include "game.hpp"
 
 int main(int argc, char **argv)
 {
-	ce::engine engine;
 	ce::log::info(ce::engine::get_version());
+
+	// This might be a bad idea
+	try
 	{
 		game g;
 		g.run();
 	}
+	catch (const std::exception &e)
+	{
+		ce::message_box::error("Error", e.what());
+	}
+
 	return 0;
 }
