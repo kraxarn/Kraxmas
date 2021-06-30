29588aa2332964f13b46110cc9599eb140285c5e
kraxarn <me@kraxarn.com>
2021-06-30 22:37:09 +0200

    Sample window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+	Conf {
+		window_title: "OpenTD: Alpha".to_owned(),
+		window_width: 1280_i32,
+		window_height: 720_i32,
+		..Default::default()
+	}
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+	let background_color = Color::from_rgba(0x21, 0x21, 0x21, 0xff);
+
+	loop {
+		clear_background(background_color);
+
+		draw_text("OpenTD: Alpha", 32_f32, 32_f32, 28_f32, WHITE);
+
+		next_frame().await
+	}
 }
