0aa78d372f4b953e247c47877a84c06cf04e1606
kraxarn <me@kraxarn.com>
2021-07-06 17:05:12 +0200

    Use Color::new instead

--- src/color.rs
+++ src/color.rs
@@ -1,17 +1,4 @@
 use macroquad::color::Color;
 
-// #ffffff
-pub const FOREGROUND: Color = Color {
-	r: (0xff / 255) as f32,
-	g: (0xff / 255) as f32,
-	b: (0xff / 255) as f32,
-	a: 1_f32,
-};
-
-// #212121
-pub const BACKGROUND: Color = Color {
-	r: (0x21 / 255) as f32,
-	g: (0x21 / 255) as f32,
-	b: (0x21 / 255) as f32,
-	a: 1_f32,
-};
+pub const FOREGROUND: Color = Color::new(1., 1., 1., 1.);
+pub const BACKGROUND: Color = Color::new(0.12, 0.12, 0.12, 1.);
